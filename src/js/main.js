4a4dc6cf327fb7078f1d2f85ecc00841b5937b40
Norberto Boa <nboa.26@gmail.com>
2023-11-17 11:46:22 +0200

    :sparkles: ADD: Add contacts section, missing form functionalities

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,24 @@ let navbar = document.querySelector('.navbar');
 let hamburg = document.querySelector('.navbar-toggler');
 const wait = (n) => new Promise((resolve) => setTimeout(resolve, n));
 
-hamburg.addEventListener('click', async function () {
-  let isOpen = hamburg.getAttribute('aria-expanded'); 
+hamburg.addEventListener('click', function () {
+  let isOpen = hamburg.getAttribute('aria-expanded');
   if (isOpen == 'false') {
     navbar.classList.remove('bg-blue');
   } else {
     navbar.classList.add('bg-blue');
   }
 });
+
+
+
+addEventListener('load' , changeNavbarBackground);
+addEventListener('scroll', changeNavbarBackground);
+
+function changeNavbarBackground() {
+  if (window.scrollY > 300) {
+    navbar.classList.add('nav-white');
+  } else {
+    navbar.classList.remove('nav-white');
+  }
+};
