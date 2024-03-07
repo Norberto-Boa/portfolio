6fe07a2169b3473877278f9751f7109bd7ffa31e
unknown <nboa.26@gmail.com>
2024-03-07 15:32:30 +0200

    feat: make contact form workable

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,7 @@ hamburg.addEventListener('click', function () {
 
 
 
-addEventListener('load' , changeNavbarBackground);
+addEventListener('load', changeNavbarBackground);
 addEventListener('scroll', changeNavbarBackground);
 
 function changeNavbarBackground() {
@@ -36,8 +36,8 @@ items.forEach((el) => {
   const minPerSlide = w < 576 ? 4 : 6;
   let next = el.nextElementSibling;
   console.log(minPerSlide);
-  for(var i = 1; i < minPerSlide; i++){
-    if(!next){
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
       next = items[0];
     }
 
@@ -46,3 +46,25 @@ items.forEach((el) => {
     next = next.nextElementSibling;
   }
 })
+
+function handleSubmit() {
+  let message
+  let errorMessage = document.querySelector("#error");
+  let successMessage = document.querySelector("#success");
+  let submitButton = document.querySelector("#submit-button");
+
+  submitButton.disabled = true;
+
+  emailjs.sendForm('service_fl5i2he', 'template_af8j9pd', "#contact")
+    .then(function (response) {
+      message = "Message was sent successfully!";
+      alert(message);
+      submitButton.disabled = false;
+      successMessage.innerHTML = message;
+    }, function (error) {
+      message = "Something went wrong";
+      alert(message);
+      submitButton.disabled = false;
+      errorMessage.innerHTML = message;
+    });
+}
